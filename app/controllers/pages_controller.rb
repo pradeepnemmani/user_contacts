d2b71e0b753c72d5e1d6e02090e96fb56a7f6c43
pradeep <pradeepnemmani@gmail.com>
2015-06-20 19:29:26 +0530

    added a custom stylesheet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
+
   def home
+  	@name = "HOME";
+  	puts "in home"
   end
 
   def contact
+  	@name = "CONTACT" 
+  	puts "in contact"
   end
-
   def about
+  	@name = "ABOUT"
+  	puts "in about"
   end
 end
